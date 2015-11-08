3c2ddae87d0e2f74270c66fe7e04014963cdeb12
Andrew Parys <andrew_parys@live.com>
2015-11-09 01:33:37 +0300

    added infrastructure for adding comments for news

--- Business/MyLiverpoolSite.Business.Services/Services/NewsCommentService.cs
+++ Business/MyLiverpoolSite.Business.Services/Services/NewsCommentService.cs
@@ -1,13 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using MyLiverpoolSite.Business.Contracts;
+using MyLiverpoolSite.Data.DataAccessLayer;
 
 namespace MyLiverpoolSite.Business.Services.Services
 {
     public class NewsCommentService : INewsCommentService
     {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public NewsCommentService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public bool AddParentComment(string comment, int newsId, int userId)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
---
+++ MyLiverpool/Controllers/NewsCommentController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using MyLiverpoolSite.Business.Contracts;
+
+namespace MyLiverpool.Controllers
+{
+    public class NewsCommentController : Controller
+    {
+        private readonly INewsCommentService _newsCommentService;
+
+        public NewsCommentController(INewsCommentService newsCommentService)
+        {
+            _newsCommentService = newsCommentService;
+        }
+
+        // GET: NewsComment
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        [HttpPost]
+        public ActionResult AddParentComment(string comment, int newsId)
+        {
+            var result = _newsCommentService.AddParentComment(comment, newsId, User.Identity.GetUserId<int>());
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
