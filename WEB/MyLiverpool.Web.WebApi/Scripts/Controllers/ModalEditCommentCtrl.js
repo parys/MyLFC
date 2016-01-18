d22229bf1c0774de28c30caf41ae3dc92ea4104e
Andrew Parys <andrew_parys@live.com>
2016-01-19 01:29:43 +0300

    Added missed files for previous commit

---
+++ WEB/MyLiverpool.Web.WebApi/Scripts/Controllers/ModalEditCommentCtrl.js
@@ -0,0 +1,17 @@
+﻿var ModalEditCommentCtrl = function ($scope, $uibModalInstance, editingComment) {
+    $scope.editingComment = editingComment;
+    $scope.oldMessage = editingComment.message;
+    $scope.oldAnswer = editingComment.answer;
+
+    $scope.ok = function () {
+        $uibModalInstance.close(editingComment);
+    };
+
+    $scope.cancel = function () {
+        $scope.editingComment.message = $scope.oldMessage;
+        $scope.editingComment.answer = $scope.oldAnswer;
+        $uibModalInstance.dismiss('cancel');
+    };
+};
+
+ModalEditCommentCtrl.$inject = ['$scope', '$uibModalInstance', 'editingComment'];
