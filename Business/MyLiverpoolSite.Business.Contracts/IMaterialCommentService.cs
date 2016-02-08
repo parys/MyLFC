9879356e74fca70c95946d2ef540a2ed983f2d7d
Andrew Parys <andrew_parys@live.com>
2016-02-09 02:22:30 +0300

    Addition to previous comment. Moved blogs to materials

---
+++ Business/MyLiverpoolSite.Business.Contracts/IMaterialCommentService.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using MyLiverpool.Business.DTO;
+using MyLiverpoolSite.Data.Entities;
+
+namespace MyLiverpoolSite.Business.Contracts
+{
+    public interface IMaterialCommentService
+    {
+        Task<int> AddCommentAsync(string message, int newsId, int? parentId, int userId, MaterialType materialType);
+
+        Task<bool> EditCommentAsync(int commentId, string message, string answer, MaterialType materialType);
+
+        Task<bool> DeleteAsync(int id, MaterialType materialType);
+
+        Task<MaterialCommentDto> AddAsync(NewsCommentEditingDto model, MaterialType materialType);
+
+        Task<bool> EditAsync(NewsCommentEditingDto model, MaterialType materialType);
+    }
+}
---
+++ Business/MyLiverpoolSite.Business.Contracts/IMaterialService.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading.Tasks;
+using MyLiverpool.Business.DTO;
+using MyLiverpoolSite.Business.ViewModels.News;
+using MyLiverpoolSite.Data.DataAccessLayer;
+using MyLiverpoolSite.Data.Entities;
+
+namespace MyLiverpoolSite.Business.Contracts
+{
+    public interface IMaterialService
+    {
+        Task<IndexNewsViewModel> GetByIdAsync(int id, MaterialType materialType);
+
+        Task<int> CreateVmAsync(CreateEditNewsViewModel model, int userId, MaterialType materialType);
+
+        Task<int> EditAsync(CreateEditNewsViewModel newsItem, MaterialType materialType);
+
+        void Delete(Material newsItem, MaterialType materialType);
+
+        Task<bool> DeleteAsync(int id, int userId, MaterialType materialType);
+
+        Task<CreateEditNewsViewModel> GetCreateEditViewModelAsync(int? id, MaterialType materialType);
+
+        Task<PageableData<IndexMiniNewsVM>> GetAllAsync(int page, int? categoryId, MaterialType materialType);
+
+        Task<bool> ActivateAsync(int id, MaterialType materialType);
+
+       // Task<IEnumerable<IndexNewsCategoryVM>> GetCategoriesAsync();
+
+        Task<PageableData<MaterialMiniDto>> GetDtoAllAsync(int page, int? categoryId, MaterialType materialType);
+
+        Task<MaterialDto> GetDtoAsync(int id, MaterialType materialType);
+
+        Task<bool> CreateAsync(MaterialDto dto, int userId, MaterialType materialType);
+
+        Task<bool> EditAsync(MaterialDto dto, int userId, MaterialType materialType);
+
+        Task<bool> AddViewAsync( int id, MaterialType materialType);
+    }
+}
