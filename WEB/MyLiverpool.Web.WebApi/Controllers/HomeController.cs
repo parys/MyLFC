ee813a0af0ac6bc7753a2807c2b0a736582b5d4e
Andrew Parys <andrew_parys@live.com>
2016-02-09 17:19:40 +0300

    Added view for aboutClub.

--- WEB/MyLiverpool.Web.WebApi/Controllers/HomeController.cs
+++ WEB/MyLiverpool.Web.WebApi/Controllers/HomeController.cs
@@ -10,18 +10,24 @@ namespace MyLiverpool.Web.WebApi.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult AboutClub()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
+        //public ActionResult About()
+        //{
+        //    ViewBag.Message = "Your application description page.";
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
+        //    return View();
+        //}
 
-            return View();
-        }
+        //public ActionResult Contact()
+        //{
+        //    ViewBag.Message = "Your contact page.";
+
+        //    return View();
+        //}
+
+
     }
 }
