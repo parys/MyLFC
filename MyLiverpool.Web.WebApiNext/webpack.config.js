666543c609db0dd246ba7a35fe1d47ea5a98861a
Andrew Parys <andrew_parys@live.com>
2016-09-28 00:29:56 +0300

    added webPack config

---
+++ MyLiverpool.Web.WebApiNext/webpack.config.js
@@ -0,0 +1,13 @@
+﻿"use strict";
+
+/// <binding ProjectOpened='Watch - Development' /> 
+
+var WebpackNotifierPlugin = require('webpack-notifier');module.exports = {    entry: {        file1: "./wwwroot/app/boot.js"    },
+    output: {        path: path.join(__dirname, "./dist"),        filename: "[name].js"    },
+    devServer: {        contentBase: ".",        host: "localhost",        port: 1669    },
+    module: {        loaders: [            {                test: /\.ts?$/,                loader: "babel-loader"            }        ]
+    },
+    plugins: [
+      new WebpackNotifierPlugin()
+    ]
+};
