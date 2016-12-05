2ea68c28c3a46533cf315c6da9e5ac14e272f7b8
parys <andrew_parys@mail.ru>
2016-12-05 16:51:51 +0300

    Added subscribing to tile, defined in routes

--- MyLiverpool.Web.WebApiNext/angular2app/app/user/user.routing.ts
+++ MyLiverpool.Web.WebApiNext/angular2app/app/user/user.routing.ts
@@ -1,11 +1,11 @@
 ﻿import { Routes }         from "@angular/router";
-import {UserDetailComponent} from "./user-detail.component";
-import {UserListComponent} from "./user-list.component";
+import { UserDetailComponent } from "./user-detail.component";
+import { UserListComponent } from "./user-list.component";
 export const userRoutes: Routes = [
-    { path: 'user', component: UserListComponent },
-    { path: 'user/list', component: UserListComponent },
-    { path: 'user/list/:page', component: UserListComponent },
-    { path: 'user/list/:page/:userName', component: UserListComponent },
-    { path: 'user/:id', component: UserDetailComponent }
+    { path: "user", component: UserListComponent, data: { title: "Пользователи" }  },
+    { path: "user/list", component: UserListComponent, data: { title: "Пользователи" }  },
+    { path: "user/list/:page", component: UserListComponent },
+    { path: "user/list/:page/:userName", component: UserListComponent },
+    { path: "user/:id", component: UserDetailComponent, data: { title: "Пользователь" }  }
   //  { path: 'news/:id/edit', component: NewsEditComponent }
 ];
