0463a502d1c904d072c1c148962d0d4da7692b79
parys <andrew_parys@mail.ru>
2017-01-19 17:51:39 +0300

    init commit for xamarin mobile apps.

---
+++ MyLiverpool.Mobile/MyLiverpool.Mobile/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MyLiverpool.Mobile
+{
+    public partial class MainPage : MasterDetailPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            masterPage.Title = "";
+            masterPage.ListView.ItemSelected += OnItemSelected;
+
+            if (Device.OS == TargetPlatform.Windows)
+            {
+                Master.Icon = "swap.png";
+            }
+        }
+
+        void OnItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var item = e.SelectedItem as MasterPageItem;
+            if (item != null)
+            {
+                Detail = new NavigationPage((Page)Activator.CreateInstance(item.TargetType));
+                masterPage.ListView.SelectedItem = null;
+                IsPresented = false;
+            }
+        }
+    }
+}
---
+++ MyLiverpool.Mobile/MyLiverpool.Mobile/RulesPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace MyLiverpool.Mobile
+{
+    public partial class RulesPage : ContentPage
+    {
+        public RulesPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
