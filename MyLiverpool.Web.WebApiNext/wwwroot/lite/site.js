6bfe06068ede83d26a5dbbb6f7ef0605db2dc745
Andrew Parys <andrew_parys@live.com>
2017-10-19 02:22:42 +0300

    Added moment js for dates for lite site

--- MyLiverpool.Web.WebApiNext/wwwroot/lite/site.js
+++ MyLiverpool.Web.WebApiNext/wwwroot/lite/site.js
@@ -1,4 +1,4 @@
-﻿window.onscroll = oEvent => {
+﻿window.onscroll= function() {
     var scrollPos = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
 
     if (scrollPos >= 200)
@@ -6,3 +6,22 @@
     else
         document.getElementById("goToTop").className = "hidden";
 };
+
+$(document).ready(function () {
+
+    $(".timeConvert").each(function () {
+        var dateTime = new Date($(this).data("date"));
+        var time;
+        if ($(this).data("sec")) {
+            time = dateTime.toString("HH:mm:ss");
+        } else {
+            time = dateTime.toString("HH:mm");
+        }
+        var date = dateTime.toString("dd/MM/YYYY");
+        if ($(this).data()) {
+
+        }//
+        //  $(this).text(`${time} ${date}`);
+        $(this).text(moment($(this).data("date")).format('DD MMM YYYY, HH:mm:ss'));
+    });
+})
