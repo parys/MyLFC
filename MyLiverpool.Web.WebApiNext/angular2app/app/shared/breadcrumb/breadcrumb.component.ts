c538f988d4df0d10539d9906cc21cba6a77cfd95
Andrew Parys <andrew_parys@live.com>
2017-11-04 01:24:18 +0300

    Removed missed console log calls. Made adding insight script only for release builds

--- MyLiverpool.Web.WebApiNext/angular2app/app/shared/breadcrumb/breadcrumb.component.ts
+++ MyLiverpool.Web.WebApiNext/angular2app/app/shared/breadcrumb/breadcrumb.component.ts
@@ -33,10 +33,6 @@ export class BreadcrumbComponent implements OnInit, OnChanges {
         this._routerSubscription = this.router.events.subscribe((navigationEnd: NavigationEnd) => {
             this.urls.length = 0; //Fastest way to clear out array
             if (!navigationEnd.urlAfterRedirects && !navigationEnd.url) return;
-            console.log(1);
-            console.log(navigationEnd);
-            console.log(navigationEnd.urlAfterRedirects);
-            console.log(navigationEnd.url);
              this.generateBreadcrumbTrail(navigationEnd.urlAfterRedirects ? navigationEnd.urlAfterRedirects : navigationEnd.url);
         });
     }
