8fb9c10ebd7a90e96e1487249c1f4dfa11ccddf3
Andrew Parys <andrew_parys@live.com>
2018-07-12 01:16:22 +0300

    Updated nugets

--- MyLfc.Common.Web/DistributedCacheManager.cs
+++ MyLfc.Common.Web/DistributedCacheManager.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using Microsoft.Extensions.Caching.Distributed;
 using MyLiverpool.Common.Utilities;
+using StackExchange.Redis;
 
 namespace MyLfc.Common.Web
 {
@@ -16,20 +17,38 @@ namespace MyLfc.Common.Web
 
         public async void SetAsync<T>(string key, T obj)
         {
-            await _distributedCache.SetAsync(key, obj.Serialize());
+            try
+            {
+                await _distributedCache.SetAsync(key, obj.Serialize());
+            }
+            catch (RedisConnectionException ex)
+            {
+            }
         }
 
         public async void SetStringAsync(string key, string obj)
         {
-            await _distributedCache.SetStringAsync(key, obj);
+            try
+            {
+                await _distributedCache.SetStringAsync(key, obj);
+            }
+            catch (RedisConnectionException ex)
+            {
+            }
         }
 
         public async Task<T> GetAsync<T>(string key)
         {
-            var cacheValue = await _distributedCache.GetAsync(key);
-            if (cacheValue != null)
+            try
             {
-                return cacheValue.Deserialize<T>();
+                var cacheValue = await _distributedCache.GetAsync(key);
+                if (cacheValue != null)
+                {
+                    return cacheValue.Deserialize<T>();
+                }
+            }
+            catch (RedisConnectionException ex)
+            {
             }
 
             return default(T);
@@ -37,29 +56,50 @@ namespace MyLfc.Common.Web
 
         public async Task<string> GetStringAsync(string key)
         {
-            return await _distributedCache.GetStringAsync(key);
+            try
+            {
+                return await _distributedCache.GetStringAsync(key);
+            }
+            catch (RedisConnectionException ex)
+            {
+            }
+
+            return null;
         }
 
         public async void RemoveAsync(string key)
         {
-            await _distributedCache.RemoveAsync(key);
+            try
+            {
+                await _distributedCache.RemoveAsync(key);
+            }
+            catch (RedisConnectionException ex)
+            {
+            }
         }
 
         public async Task<T> GetOrCreateAsync<T>(string key, Func<Task<T>> method)
         {
             T result;
-            var cached = await _distributedCache.GetAsync(key);
-            if (cached != null)
+            try
             {
-                result = cached.Deserialize<T>();
+                var cached = await _distributedCache.GetAsync(key);
+                if (cached != null)
+                {
+                    result = cached.Deserialize<T>();
+                }
+                else
+                {
+                    result = await (Task<T>)method.DynamicInvoke();
+                    if (result != null)
+                    {
+                        await _distributedCache.SetAsync(key, result.Serialize());
+                    }
+                }
             }
-            else
+            catch (RedisConnectionException ex)
             {
                 result = await (Task<T>)method.DynamicInvoke();
-                if (result != null)
-                {
-                    await _distributedCache.SetAsync(key, result.Serialize());
-                }
             }
 
             return result;
@@ -67,20 +107,28 @@ namespace MyLfc.Common.Web
 
         public async Task<string> GetOrCreateStringAsync(string key, Func<Task<string>> method)
         {
-            var cached = await _distributedCache.GetStringAsync(key);
-            if (cached != null)
+            string result;
+            try
             {
-                return cached;
+                var cached = await _distributedCache.GetStringAsync(key);
+                if (cached != null)
+                {
+                    return cached;
+                }
+
+                result = await (Task<string>)method.DynamicInvoke();
+                if (string.IsNullOrWhiteSpace(result))
+                {
+                    await _distributedCache.RemoveAsync(key);
+                }
+                else
+                {
+                    await _distributedCache.SetStringAsync(key, result);
+                }
             }
-            
-            var result = await(Task<string>)method.DynamicInvoke();
-            if (string.IsNullOrWhiteSpace(result))
+            catch (RedisConnectionException ex)
             {
-                await _distributedCache.RemoveAsync(key);
-            }
-            else
-            {
-                await _distributedCache.SetStringAsync(key, result);
+                result = await (Task<string>)method.DynamicInvoke();
             }
 
             return result;
