976a0f9385d777695c47122a9a9da1a703a5809c
Andrew Parys <andrew_parys@live.com>
2019-08-22 01:38:08 +0300

    Small improvements to styles.

--- src/MyLiverpool.Web.WebApiNext/ClientApp/src/app/home/navbar/navbar.component.ts
+++ src/MyLiverpool.Web.WebApiNext/ClientApp/src/app/home/navbar/navbar.component.ts
@@ -1,28 +1,31 @@
-﻿import { Component, Output, EventEmitter, ChangeDetectionStrategy, PLATFORM_ID, Inject, HostListener } from "@angular/core";
-import { isPlatformBrowser } from "@angular/common";
+﻿import { Component, Output, EventEmitter, ChangeDetectionStrategy, PLATFORM_ID, Inject, HostListener } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
-    selector: "navbar",
-    templateUrl: "./navbar.component.html",
-    styleUrls: ["./navbar.component.scss"],
+    selector: 'navbar',
+    templateUrl: './navbar.component.html',
+    styleUrls: ['./navbar.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class NavbarComponent {
     @Output()
     public toggle: EventEmitter<any> = new EventEmitter();
-    public showAd: boolean = false;
+    public showAd = false;
 
-    @HostListener("window:resize", ["$event"])
+    @HostListener('window:resize', ['$event'])
     public sizeChange(event: any) {
         if (isPlatformBrowser(this.platformId)) {
-            this.showAd = window.innerWidth > 1673;
+            const showAd = window.innerWidth > 1673;
+            if (showAd !== this.showAd) {
+                this.showAd = showAd;
+            }
         }
     }
 
     constructor(
         @Inject(PLATFORM_ID) private platformId: Object) {
         if (isPlatformBrowser(this.platformId)) {
-            this.sizeChange("");
+            this.sizeChange('');
         }
     }
 }
