6bb679caed36fe33cf3182ab94e97dd49f5c8908
Andrew Parys <andrew_parys@live.com>
2019-09-23 00:37:06 +0300

    #242 load epl table only when need.

--- src/MyLiverpool.Web.WebApiNext/ClientApp/src/app/layout/components/eplTable/eplTable.component.ts
+++ src/MyLiverpool.Web.WebApiNext/ClientApp/src/app/layout/components/eplTable/eplTable.component.ts
@@ -1,9 +1,9 @@
-﻿import { Component, OnInit, OnDestroy, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
-
-import { Subscription } from 'rxjs';
+﻿import { Component, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
 
 import { HelperType } from '@domain/models';
+import { ObserverComponent } from '@domain/base';
 import { RolesCheckedService } from '@base/auth';
+
 import { LayoutService } from '@layout/layout.service';
 
 @Component({
@@ -12,38 +12,41 @@ import { LayoutService } from '@layout/layout.service';
     styleUrls: ['./eplTable.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class EplTableComponent implements OnInit, OnDestroy {
-    private sub: Subscription;
-    private sub2: Subscription;
+export class EplTableComponent extends ObserverComponent {
     public eplTable: string;
     public opened = false;
 
     constructor(private service: LayoutService,
                 private cd: ChangeDetectorRef,
                 public roles: RolesCheckedService) {
+        super();
     }
 
-    public ngOnInit(): void {
-        this.sub = this.service
-            .getValue(HelperType.EplTable)
-            .subscribe((data: string) => this.eplTable = data,
-                null,
-                () => {
-                    this.cd.markForCheck();
-                });
+    public open(): void {
+        this.opened = !this.opened;
+        if (this.opened) {
+            this.init();
+        }
     }
 
-    public ngOnDestroy(): void {
-        if (this.sub) { this.sub.unsubscribe(); }
-        if (this.sub2) { this.sub2.unsubscribe(); }
+    private init() {
+        const sub$ = this.service
+        .getValue(HelperType.EplTable)
+        .subscribe((data: string) => this.eplTable = data,
+            null,
+            () => {
+                this.cd.markForCheck();
+            });
+        this.subscriptions.push(sub$);
     }
 
     public updateEplTable(): void {
-        this.sub2 = this.service
+        const sub$ = this.service
             .updateEplTable()
             .subscribe((data: string) => {
                 this.eplTable = data;
                 this.cd.markForCheck();
             });
+        this.subscriptions.push(sub$);
     }
 }
