5ba1d948ddf30e8b3ad41a1e6b15a29499a72239
Andrew Parys <andrew_parys@live.com>
2019-11-09 10:51:01 +0300

    minor improves

--- src/MyLiverpool.Web.WebApiNext/ClientApp/src/app/polls/lazy/poll.routes.ts
+++ src/MyLiverpool.Web.WebApiNext/ClientApp/src/app/polls/lazy/poll.routes.ts
@@ -1,16 +1,17 @@
-﻿import { Routes } from "@angular/router";
-import { PollListComponent } from "./poll-list/poll-list.component";
-import { PollDetailComponent } from "./poll-detail/poll-detail.component";
+﻿import { Routes } from '@angular/router';
+
+import { PollListComponent } from './poll-list/poll-list.component';
+import { PollDetailComponent } from './poll-detail/poll-detail.component';
 
 export const pollRoutes: Routes = [
-    { path: "", component: PollListComponent, data: { title: "Опросы" } },
+    { path: '', component: PollListComponent, data: { title: 'Опросы' } },
     {
-        path: ":id",
+        path: ':id',
         children: [
-            { path: "", component: PollDetailComponent, data: { title: "Опрос" } },
+            { path: '', component: PollDetailComponent, data: { title: 'Опрос' } },
             {
-                path: "edit",
-                loadChildren: "./+poll-edit/poll-edit.module#PollEditModule"
+                path: 'edit',
+                loadChildren: () => import('./+poll-edit/poll-edit.module').then(m => m.PollEditModule)
             }
         ]
     }
