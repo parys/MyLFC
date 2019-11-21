633644d17aac2ad8e857a6ffa7397fbdec4665f0
Michael Prentice <splaktar@gmail.com>
2019-11-20 20:16:42 -0500

    ng update @angular/material --migrate-only --from 8 --to 9

--- src/MyLiverpool.Web.WebApiNext/ClientApp/src/app/app.module.browser.ts
+++ src/MyLiverpool.Web.WebApiNext/ClientApp/src/app/app.module.browser.ts
@@ -1,11 +1,12 @@
 ﻿import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ServiceWorkerModule } from '@angular/service-worker';
-import { BrowserTransferStateModule } from '@angular/platform-browser';
+import { BrowserTransferStateModule, HammerModule, HAMMER_GESTURE_CONFIG } from '@angular/platform-browser';
 
 import { environment } from '@environments/environment';
 import { AppModuleShared } from './app.module';
 import { AppComponent } from './app.component';
+import { GestureConfig } from "../gesture-config";
 
 @NgModule({
     bootstrap: [AppComponent],
@@ -13,11 +14,12 @@ import { AppComponent } from './app.component';
         AppModuleShared,
         BrowserAnimationsModule,
         BrowserTransferStateModule,
-        ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
+        ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production }),
+        HammerModule
     ],
     providers: [
         //   { provide: 'BASE_URL', useFactory: getBaseUrl }
-    ]
+    { provide: HAMMER_GESTURE_CONFIG, useClass: GestureConfig }]
 })
 export class AppModule {
 }
