cf5891a9010aa2e0c120f7ce4b20e3b4ff24bab9
Andrei Kapitanchyk <andrei.kapitanchyk@h2g2.is>
2020-12-14 18:56:25 +0300

    Removed uneeded dependencies from tests project.

---
+++ src/MyLfc.Common.Web/SignalrEntity.cs
@@ -0,0 +1,16 @@
+﻿namespace MyLfc.Common.Web
+{
+    public class SignalrEntity<T> where T: class
+    {
+        public ActionType Type { get; set; }
+
+        public T Entity { get; set; }
+    }
+
+    public enum ActionType : int
+    {
+        Add = 1,
+        Update = 2,
+        Delete = 3
+    }
+}
