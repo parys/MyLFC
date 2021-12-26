700db0c0165f7d7aa00d5a3a7853b6acbea5d8ac
Andrei Kapitanchyk <andrei.kapitanchyk@h2g2.is>
2021-12-26 10:06:37 +0100

    Added test for create person handler

---
+++ test/MyLfc.Application.Tests/Features/Persons/CreatePersonCommand/CreatePersonCommandCollection.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using AutoFixture;
+using MyLfc.Application.Tests.Infrastructure;
+using MyLfc.Application.Tests.Infrastructure.Customizations.Domains;
+using MyLfc.Application.Tests.Infrastructure.Seeds;
+using MyLfc.Domain;
+using MyLiverpool.Data.Common;
+using Xunit;
+
+namespace MyLfc.Application.Tests.Features.Persons.CreatePersonCommand
+{
+    [CollectionDefinition(nameof(CreatePersonCommandCollection))]
+    public class CreatePersonCommandCollection : ICollectionFixture<CreatePersonCommandTestFixture> { }
+
+    public class CreatePersonCommandTestFixture : BaseTestFixture
+    {
+        public static int DeletedMaterialId;
+        public static int PendingMaterialId;
+        public static List<Person> Persons { get; private set; }
+        
+        public CreatePersonCommandTestFixture()
+        {
+            //MaterialCategorySeeder.Seed(Context);
+            //Materials = MaterialSeeder.Seed(Context);
+            //UserSeeder.Seed(Context);
+            //SeedMaterials();
+        }
+
+        private void SeedMaterials()
+        {
+            //var deletedMaterial = new Fixture()
+            //    .Customize(new MaterialCustomization(MaterialType.News, true))
+            //    .Create<Material>();
+            //Context.Materials.Add(deletedMaterial);
+
+            //var pendingMaterial = new Fixture()
+            //    .Customize(new MaterialCustomization(MaterialType.News, false))
+            //    .Create<Material>();
+            //pendingMaterial.Pending = true;
+            //Context.Materials.Add(pendingMaterial);
+
+            //Context.SaveChanges();
+
+            //DeletedMaterialId = deletedMaterial.Id;
+            //PendingMaterialId = pendingMaterial.Id;
+        }
+    }
+}
---
+++ test/MyLfc.Application.Tests/Features/Persons/CreatePersonCommand/HandlerTests.cs
@@ -0,0 +1,45 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoFixture;
+using FluentAssertions;
+using MediatR;
+using Moq;
+using MyLfc.Application.Tests.Infrastructure.Extensions;
+using MyLfc.Application.Tests.Pms.CreatePmCommand;
+using MyLfc.Common.Web.Hubs;
+using MyLiverpool.Business.Contracts;
+using Xunit;
+using Handler = MyLfc.Application.Persons.CreatePersonCommand.Handler;
+using Request = MyLfc.Application.Persons.CreatePersonCommand.Request;
+using Response = MyLfc.Application.Persons.CreatePersonCommand.Response;
+
+namespace MyLfc.Application.Tests.Features.Persons.CreatePersonCommand
+{
+    [Collection(nameof(CreatePersonCommandCollection))]
+    public class HandlerTests
+    {
+        private readonly ILiverpoolContext _context;
+        private readonly IRequestHandler<Request, Response> _handler;
+
+        public HandlerTests(CreatePersonCommandTestFixture fixture)
+        {
+            _context = fixture.Context;
+            _handler = new Handler(fixture.Context, fixture.Mapper);
+        }
+
+        [Fact]
+        public async Task CreatePerson_WhenObjectIsCorrect_ReturnsCreatedPerson()
+        {
+            var request = new Fixture()
+                .Create<Request>();
+           
+            var result = await _handler.Handle(request, CancellationToken.None);
+
+            result.Id.Should().BeGreaterThan(0);
+
+            result.ArePublicInstancePropertiesEqual(request);
+        }
+
+    }
+}
